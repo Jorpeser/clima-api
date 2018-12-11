59a77dd0e91f72697bc7acaa313c7c70aba3cc65
Jaime Laborda <jaimelaborda@gmail.com>
2018-12-11 19:14:45 +0100

    Añado metodo para mostrar el último record de la tabla

--- src/app/routes/clima.js
+++ src/app/routes/clima.js
@@ -5,6 +5,18 @@ module.exports = app => {
   const connection = dbConnection();
 
   app.get('/clima', (req, res) => {
+    connection.query('SELECT * FROM clima ORDER BY id DESC LIMIT 1 ', (err, result) => {
+      if(err){
+        console.log(err);
+      }else{
+        res.send({
+          clima: result
+        });
+      }
+    });
+  });
+
+  app.get('/clima-all', (req, res) => {
     connection.query('SELECT * FROM clima', (err, result) => {
       if (err){
         console.log(err);
@@ -24,9 +36,18 @@ module.exports = app => {
         humedad
       }
     , (err, result) => {
-      res.send({
-        result: result
-      });
+      if(err){
+        res.send({
+          error: 'Ha habido un error en la consulta a la base de datos'
+        })
+      }else{
+        res.send({
+          result: result,
+          temperatura: temperatura,
+          humedad: humedad
+        });
+      }
+      
     });
   });
 };
